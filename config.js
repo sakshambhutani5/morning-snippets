4d921f612a98fba12fca78094d0ebf5831e62527
sakshambhutani5 <62626981+sakshambhutani5@users.noreply.github.com>
2025-03-16 00:24:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 const API_KEY = "f6bf806baa42405dece46b5f18121d38";  // Replace with your actual API key
+
+export const RAPIDAPI_KEY = "41381fab3cmshee1841270b11804p160bd4jsn388b3ce0a0c6";  // Replace with actual RapidAPI key
